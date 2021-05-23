c67b4570adfdab216e051b39951da7a69c3984a2
Lucas Santos <lucashfreitas1@gmail.com>
2021-05-24 00:59:11 +1200

    fix: project setup

--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,30 +1,59 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/jsx-no-comment-textnodes */
+import { themeGet } from '@styled-system/theme-get';
+import Image from 'next/image';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { HeaderData } from '../../data/global/header';
+import useScrollPosition from '../../hooks/useScrollPosition';
 import Nav from './nav';
 
-const HeaderWrapper = styled.header`
+const HeaderWrapper = styled.header<{ scroll?: boolean }>`
   position: fixed;
   top: 0;
   left: 0;
-  height: 75px;
+
+  ${(props) =>
+    props.scroll &&
+    css`
+      transform: translateY(-100px);
+      background: ${themeGet('components.header.background')};
+    `};
+
   width: 100%;
+
+  transition: transform 0.4s;
+
   .container {
+    max-width: 90%;
+    margin: 0 auto;
+    height: 7.5rem;
     display: flex;
+  }
+
+  .logo {
+    flex-shrink: 1;
+    width: 100%;
+    height: 100%;
+    position: relative;
   }
 `;
 
-const LogoImg = styled.img``;
-
 const Header: React.FunctionComponent<HeaderData> = (props) => {
+  const scroll = useScrollPosition();
   const { logo, menu } = props;
 
   return (
-    <HeaderWrapper>
+    <HeaderWrapper scroll={scroll.y > 0}>
       <div className="container">
-        <LogoImg src={logo} />
+        <div className="logo">
+          <Image
+            className="logo"
+            layout="fill"
+            objectFit="contain"
+            src={logo}
+          />
+        </div>
 
         <Nav></Nav>
       </div>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,14 @@
+const components = {
+  header: {
+    background: 'rgb(6, 186, 191)',
+  },
+};
+
 const theme = {
   colors: {
     primary: 'rgb(225, 108, 73)',
   },
+  components,
 };
 
 export default theme;
